832f6e9f68ae4890e2b0dfb96709e203796a9839
João <cjoaopedrocorbari@gmail.com>
2026-08-25 23:41:17 -0300

    Adiciona exercício 006: cálculo de dobro, triplo e raiz quadrada

---
+++ ex006.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um número: '))
+print(f'o dobro de {n} vale {n*2}')
+print(f'o triplo de {n} vale {n*3}')
+print(f'a raiz quadrada de {n} vale {n**0.5}')
+# toto me ajudou na raiz quadrada ali tmj!
