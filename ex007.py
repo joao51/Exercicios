418bfa91c1dd32b6c69b6e750552a8db86c5be67
João <cjoaopedrocorbari@gmail.com>
2026-08-26 00:01:39 -0300

    Adiciona exercício 007: cálculo da média de três notas

---
+++ ex007.py
@@ -0,0 +1,5 @@
+n1 = float(input('Primeira nota do aluno: '))
+n2 = float(input('Segunda nota do aluno: '))
+n3 = float(input('Tercerira nota do aluno: '))
+m = (n1 + n2 + n3) /3
+print(f'A média entre {n1}, {n2} e {n3} é engual a {m:.2f}')
