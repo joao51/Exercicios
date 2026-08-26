b641b15885f623f1eb73f492a766161bebb2d7f0
João <cjoaopedrocorbari@gmail.com>
2026-08-26 00:21:45 -0300

    Ajusta a formatação da média para exibir uma casa decimal

--- ex007.py
+++ ex007.py
@@ -2,4 +2,4 @@ n1 = float(input('Primeira nota do aluno: '))
 n2 = float(input('Segunda nota do aluno: '))
 n3 = float(input('Tercerira nota do aluno: '))
 m = (n1 + n2 + n3) /3
-print(f'A média entre {n1}, {n2} e {n3} é engual a {m:.2f}')
+print(f'A média entre {n1:.1f}, {n2:.1f} e {n3:.1f} é engual a {m:.1f}')
